e116717e9be245d70c747c8897638b4eb97f702e
taku <tk.nishi.1018@gmail.com>
2021-03-10 23:05:21 +0900

    :hammer: FIX: 通知へのアクセス許可がない場合、権限管理画面に遷移するように変更。

--- AndroidApp/app/src/main/java/me/nya_n/notificationnotifier/views/activities/MainActivity.kt
+++ AndroidApp/app/src/main/java/me/nya_n/notificationnotifier/views/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package me.nya_n.notificationnotifier.views.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import me.nya_n.notificationnotifier.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        permissionCheck()
+    }
+
+    private fun permissionCheck() {
+        if (NotificationManagerCompat.getEnabledListenerPackages(this)
+                .contains(packageName)) {
+            return
+        }
+        AlertDialog.Builder(this)
+            .setMessage(R.string.require_permission)
+            .setPositiveButton(R.string.next) { _, _ ->
+                Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS).apply {
+                    startActivity(this)
+                }
+            }
+            .show()
     }
 }
