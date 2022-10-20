61fc564007d2da1f51b144bafdeb954f762faf56
taku <tk.nishi.1018@gmail.com>
2022-10-21 04:21:31 +0900

    gradleの修正

--- AndroidApp/ui/build.gradle.kts
+++ AndroidApp/ui/build.gradle.kts
@@ -9,6 +9,30 @@ plugins {
 android {
     compileSdk = 33
 
+    defaultConfig {
+        minSdk = 30
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
     buildFeatures {
         dataBinding = true
     }
