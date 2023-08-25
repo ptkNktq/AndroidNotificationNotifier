eb7b94093659afa0a112f3381d9bca155b8d6b46
taku <tk.nishi.1018@gmail.com>
2023-08-25 23:20:51 +0900

    不要になったプロパティの削除

--- AndroidApp/domain/build.gradle.kts
+++ AndroidApp/domain/build.gradle.kts
@@ -8,8 +8,6 @@ android {
 
     defaultConfig {
         minSdk = Versions.MIN_SDK
-        targetSdk = Versions.TARGET_SDK
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
