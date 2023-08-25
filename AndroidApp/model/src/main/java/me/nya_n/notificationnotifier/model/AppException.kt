947ba5c8bf981ee914c7454b5b2d2894e6281449
taku <tk.nishi.1018@gmail.com>
2023-08-25 22:06:43 +0900

    不要なstringリソースの削除

--- AndroidApp/model/src/main/java/me/nya_n/notificationnotifier/model/AppException.kt
+++ AndroidApp/model/src/main/java/me/nya_n/notificationnotifier/model/AppException.kt
@@ -1,21 +1,9 @@
 package me.nya_n.notificationnotifier.model
 
-import androidx.annotation.StringRes
-
 sealed class AppException : Exception() {
-    /**
-     * エラーテキストのリソースID
-     */
-    @get:StringRes
-    abstract val errorTextResourceId: Int
+    /** アドレスの形式が不正 */
+    class InvalidAddrException : AppException()
 
-    class InvalidAddrException : AppException() {
-        override val errorTextResourceId: Int
-            get() = R.string.validation_error_addr
-    }
-
-    class PermissionDeniedException : AppException() {
-        override val errorTextResourceId: Int
-            get() = R.string.permission_denied
-    }
+    /** 権限がない */
+    class PermissionDeniedException : AppException()
 }
