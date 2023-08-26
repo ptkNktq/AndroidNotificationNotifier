a3ab7c8ea6504de47bede8257dd7d07b1796c6a7
taku <tk.nishi.1018@gmail.com>
2023-08-27 08:58:07 +0900

    composeバージョン更新

--- AndroidApp/buildSrc/src/main/java/Versions.kt
+++ AndroidApp/buildSrc/src/main/java/Versions.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val COMPILE_SDK = 33
-    const val TARGET_SDK = 33
+    const val COMPILE_SDK = 34
+    const val TARGET_SDK = 34
     const val MIN_SDK = 30
     val JAVA_VERSION = JavaVersion.VERSION_17
     const val JVM_TARGET = "17"
