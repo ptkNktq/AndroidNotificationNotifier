47b63520fcee47a93e9209798c04421b3a9a04e1
taku <tk.nishi.1018@gmail.com>
2023-09-03 05:12:34 +0900

    ダミーの追加

---
+++ AndroidApp/domain/src/main/java/me/nya_n/notificationnotifier/domain/usecase/dummy/LoadAppUseCaseDummyImpl.kt
@@ -0,0 +1,24 @@
+package me.nya_n.notificationnotifier.domain.usecase.dummy
+
+import android.content.pm.PackageManager
+import me.nya_n.notificationnotifier.domain.usecase.LoadAppUseCase
+import me.nya_n.notificationnotifier.model.InstalledApp
+
+class LoadAppUseCaseDummyImpl : LoadAppUseCase {
+    override suspend fun invoke(pm: PackageManager): Result<LoadAppUseCase.Outputs> {
+        return Result.success(
+            LoadAppUseCase.Outputs(
+                listOf(
+                    InstalledApp("サンプル アプリ１", "me.nya_n.notificationnotifier"),
+                    InstalledApp("サンプル アプリ２", "me.nya_n.notificationnotifier"),
+                    InstalledApp("サンプル アプリ３", "me.nya_n.notificationnotifier"),
+                    InstalledApp("サンプル アプリ４", "me.nya_n.notificationnotifier"),
+                    InstalledApp("サンプル アプリ５", "me.nya_n.notificationnotifier"),
+                    InstalledApp("サンプル アプリ６", "me.nya_n.notificationnotifier"),
+                    InstalledApp("サンプル アプリ７", "me.nya_n.notificationnotifier"),
+                ),
+                emptyList()
+            )
+        )
+    }
+}
