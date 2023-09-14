cac65d5005bc3d674f6c042124eab04fd4d94034
taku <tk.nishi.1018@gmail.com>
2023-09-15 04:04:41 +0900

    repositories定義の制限追加

--- AndroidApp/settings.gradle.kts
+++ AndroidApp/settings.gradle.kts
@@ -6,8 +6,9 @@ pluginManagement {
     }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
