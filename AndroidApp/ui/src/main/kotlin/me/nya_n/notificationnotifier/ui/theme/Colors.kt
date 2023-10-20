b0a18d5f099490c71199e407ac1a5db6a9ae4160
taku <tk.nishi.1018@gmail.com>
2023-10-21 06:52:19 +0900

    BottomNavigationをM3のものに移行

--- AndroidApp/ui/src/main/kotlin/me/nya_n/notificationnotifier/ui/theme/Colors.kt
+++ AndroidApp/ui/src/main/kotlin/me/nya_n/notificationnotifier/ui/theme/Colors.kt
@@ -4,8 +4,11 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
+    // 配色アイデア手帖 p186 レシピ073 をベースにした色
+    val gray = Color(0xFFCECECE)
     val RoseBrown = Color(0xFFC7B5A8)
     val Brown = Color(0xFF443731)
+    val LightBrown = Color(0xFF817167)
     val BasicBlack = Color(0xFF352E2B)
 }
 
@@ -13,5 +16,10 @@ val AppColorScheme = darkColorScheme(
     primary = AppColors.Brown,
     onPrimary = Color.White,
     secondary = AppColors.RoseBrown,
-    onSecondary = AppColors.BasicBlack
+    onSecondary = AppColors.BasicBlack,
+    secondaryContainer = AppColors.LightBrown,
+    onSecondaryContainer = Color.White,
+    surface = AppColors.Brown,
+    onSurface = Color.White,
+    onSurfaceVariant = AppColors.gray
 )
