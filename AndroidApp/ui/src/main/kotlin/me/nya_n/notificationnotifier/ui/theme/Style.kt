312e143b52400bb95db66571e1dad2e11584fcad
taku <tk.nishi.1018@gmail.com>
2024-06-06 07:44:03 +0900

    material.LocalContentColor → material3.LocalContentColor

--- AndroidApp/ui/src/main/kotlin/me/nya_n/notificationnotifier/ui/theme/Style.kt
+++ AndroidApp/ui/src/main/kotlin/me/nya_n/notificationnotifier/ui/theme/Style.kt
@@ -1,20 +1,16 @@
 package me.nya_n.notificationnotifier.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.LocalContentColor
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
-import androidx.compose.material.MaterialTheme as M2
 
 @Composable
-@Suppress("UNUSED_PARAMETER")
 fun AppTheme(
-    isDarkMode: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
@@ -37,7 +33,7 @@ object WhiteRippleTheme : RippleTheme {
     override fun rippleAlpha(): RippleAlpha {
         return RippleTheme.defaultRippleAlpha(
             contentColor = LocalContentColor.current,
-            lightTheme = M2.colors.isLight
+            lightTheme = false // AppColorSchemeがdarkColorSchemeのみサポートしている
         )
     }
 }
