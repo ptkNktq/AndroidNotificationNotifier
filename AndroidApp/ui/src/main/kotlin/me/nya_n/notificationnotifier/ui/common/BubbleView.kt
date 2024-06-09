72b66b7d225e0c720771c33178c90d3bffae79a1
taku <tk.nishi.1018@gmail.com>
2024-06-10 08:44:02 +0900

    BubbleViewの追加

---
+++ AndroidApp/ui/src/main/kotlin/me/nya_n/notificationnotifier/ui/common/BubbleView.kt
@@ -0,0 +1,115 @@
+package me.nya_n.notificationnotifier.ui.common
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Paint
+import android.graphics.Shader
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+@SuppressLint("ViewConstructor")
+class BubbleView(
+    context: Context,
+    private val screenWidth: Float,
+    private val screenHeight: Float
+) : SurfaceView(context), SurfaceHolder.Callback {
+
+    companion object {
+        private const val MAX_CIRCLE = 7
+    }
+
+    private val r = Random(System.currentTimeMillis())
+    private val coroutineScope = CoroutineScope(Job())
+    private val backgroundPaint = Paint().apply {
+        shader = LinearGradient(
+            0f,
+            0f,
+            screenWidth,
+            screenHeight,
+            Color.parseColor("#FF8B67"),
+            Color.parseColor("#FFAE5B"),
+            Shader.TileMode.CLAMP
+        )
+        isAntiAlias = true
+    }
+    private val circlePaint = Paint().apply {
+        color = Color.parseColor("#44ffffff")
+        isAntiAlias = true
+    }
+    private val circles = Array(MAX_CIRCLE) { Circle(r, screenWidth, screenHeight) }
+
+    init {
+        setUp()
+    }
+
+    private fun setUp() {
+        holder.addCallback(this)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        // noop
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        coroutineScope.launch {
+            while (true) {
+                val canvas = holder.lockCanvas()
+                canvas.drawPaint(backgroundPaint)
+                circles.forEach {
+                    it.draw(canvas, circlePaint)
+                    it.update()
+                }
+                holder.unlockCanvasAndPost(canvas)
+            }
+        }
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // FIXME: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.graphics.Canvas.drawPaint(android.graphics.Paint)' on a null object reference
+        coroutineScope.cancel()
+    }
+}
+
+class Circle(
+    private val r: Random,
+    private val screenWidth: Float,
+    private val screenHeight: Float,
+    private val isPortrait: Boolean = true
+) {
+    private var size: Float = 0f
+    private var x: Float = 0f
+    private var y: Float = 0f
+    private var speed: Float = 0f
+
+    init {
+        reset()
+    }
+
+    private fun reset() {
+        size = (r.nextInt((screenWidth / if (isPortrait) 3 else 5).toInt()) + 48).toFloat()
+        speed = (r.nextFloat() + r.nextFloat()) % 6 + 1
+        x = r.nextInt(screenWidth.toInt()).toFloat()
+        y = screenHeight + size + r.nextInt(screenHeight.toInt())
+    }
+
+    fun draw(canvas: Canvas, paint: Paint) {
+        canvas.drawCircle(
+            x, y, size / 2, paint
+        )
+    }
+
+    fun update() {
+        y -= speed
+        if (y + size < 0) {
+            reset()
+        }
+    }
+}
