a4941eef99e65b35ff2c54adf4dc30ecf4a19c51
taku <tk.nishi.1018@gmail.com>
2024-06-19 01:07:13 +0900

    agp 8.2.0 -> 8.5.0

--- AndroidApp/build-logic/src/main/kotlin/me/nya_n/notificationnotifier/Compose.kt
+++ AndroidApp/build-logic/src/main/kotlin/me/nya_n/notificationnotifier/Compose.kt
@@ -4,7 +4,7 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 
 internal fun Project.configureCompose(
-    extension: CommonExtension<*, *, *, *, *>
+    extension: CommonExtension<*, *, *, *, *, *>
 ) {
     extension.apply {
         buildFeatures {
