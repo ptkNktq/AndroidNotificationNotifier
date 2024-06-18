a7b90daecdaedffe48b9a4ac2b8e982b0735c0a5
taku <tk.nishi.1018@gmail.com>
2024-06-19 01:07:33 +0900

    Unable to make progress running work.対策

--- AndroidApp/build.gradle.kts
+++ AndroidApp/build.gradle.kts
@@ -5,3 +5,8 @@ plugins {
     alias(libs.plugins.com.google.devtools.ksp) apply false
     alias(libs.plugins.com.jaredsburrows.license) apply false
 }
+
+// https://github.com/facebook/react-native/issues/44501#issuecomment-2112926438
+gradle.startParameter.excludedTaskNames.addAll(
+    gradle.startParameter.taskNames.filter { it.contains("testClasses") }
+)
