934aa27f211bfc0c10030ef4ac1124eedd687bfa
taku <tk.nishi.1018@gmail.com>
2024-10-15 02:32:03 +0900

    コンポーネントレベルのスクショテストの追加

--- AndroidApp/ui/src/screenshotTest/kotlin/me/nya_n/notificationnotifier/ScreenshotTest.kt
+++ AndroidApp/ui/src/screenshotTest/kotlin/me/nya_n/notificationnotifier/ScreenshotTest.kt
@@ -10,6 +10,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import me.nya_n.notificationnotifier.model.InstalledApp
+import me.nya_n.notificationnotifier.ui.common.AppList
+import me.nya_n.notificationnotifier.ui.common.AppOutlinedButton
+import me.nya_n.notificationnotifier.ui.common.AppScaffold
+import me.nya_n.notificationnotifier.ui.common.Category
+import me.nya_n.notificationnotifier.ui.common.CommonDialog
+import me.nya_n.notificationnotifier.ui.common.EmptyView
+import me.nya_n.notificationnotifier.ui.common.RequireNotificationPermissionDialog
+import me.nya_n.notificationnotifier.ui.common.RequirePackageVisibilityDialog
+import me.nya_n.notificationnotifier.ui.common.TopBar
 import me.nya_n.notificationnotifier.ui.screen.detail.DetailContent
 import me.nya_n.notificationnotifier.ui.screen.main.MainContent
 import me.nya_n.notificationnotifier.ui.screen.main.TabItem
@@ -111,4 +120,121 @@ class ScreenshotTest {
     // endregion
 
     // region: コンポーネントレベル
+
+    @Preview(backgroundColor = 0xFFC7B5A8, showBackground = true)
+    @Composable
+    fun AppListComponentScreenshotTest() {
+        val items = listOf(
+            InstalledApp("Sample App", "me.nya_n.notificationnotifier"),
+            InstalledApp(
+                "Sample App Name So Looooooooooooooooong",
+                "me.nya_n.notificationnotifier"
+            ),
+        )
+        AppTheme {
+            AppList(
+                items = items,
+                onAppSelected = { }
+            )
+        }
+    }
+
+    @Preview(backgroundColor = 0xFFC7B5A8, showBackground = true)
+    @Composable
+    fun EmptyAppListComponentScreenshotTest() {
+        AppTheme {
+            AppList(
+                items = emptyList(),
+                onAppSelected = { }
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun AppOutlinedButtonComponentScreenshotTest() {
+        AppTheme {
+            AppOutlinedButton("text") { }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun AppScaffoldComponentScreenshotTest() {
+        val snackbarHostState = remember { SnackbarHostState() }
+        AppTheme {
+            AppScaffold(snackbarHostState) { }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun CategoryComponentScreenshotTest() {
+        AppTheme {
+            Category("カテゴリ")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun CommonDialogComponentScreenshotTest() {
+        AppTheme {
+            CommonDialog(
+                message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                positiveButtonText = "YES",
+                negativeButtonText = "NO",
+                onPositiveDismissRequest = { },
+                onNegativeDismissRequest = { }
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun RequireNotificationPermissionDialogComponentScreenshotTest() {
+        AppTheme {
+            RequireNotificationPermissionDialog(
+                onDismissRequest = { }
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun RequirePackageVisibilityDialogComponentScreenshotTest() {
+        AppTheme {
+            RequirePackageVisibilityDialog(
+                onDismissRequest = { }
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun EmptyViewComponentScreenshotTest() {
+        AppTheme {
+            EmptyView(message = "empty")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun TopBarComponentScreenshotTest() {
+        AppTheme {
+            TopBar()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun SubContentTopBarComponentScreenshotTest() {
+        AppTheme {
+            TopBar(
+                hasBackContent = true,
+                onBack = { }
+            )
+        }
+    }
+
+    // endregion
 }
