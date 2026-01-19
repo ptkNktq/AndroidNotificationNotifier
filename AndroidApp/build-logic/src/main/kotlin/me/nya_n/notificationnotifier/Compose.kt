435155e58d14211474ca0be24df2f088e5eac323
Takuya Nishizawa <tk.nishi.1018@gmail.com>
2026-01-20 01:42:09 +0900

    compose用のconfigure修正

--- AndroidApp/build-logic/src/main/kotlin/me/nya_n/notificationnotifier/Compose.kt
+++ AndroidApp/build-logic/src/main/kotlin/me/nya_n/notificationnotifier/Compose.kt
@@ -4,10 +4,10 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 
 internal fun Project.configureCompose(
-    extension: CommonExtension<*, *, *, *, *, *>
+    extension: CommonExtension
 ) {
     extension.apply {
-        buildFeatures {
+        buildFeatures.apply {
             compose = true
         }
     }
